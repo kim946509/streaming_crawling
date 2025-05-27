c5c63d7efde87272de993eac45fa8a8754bb246b
민경쓰 <bmk0703@naver.com>
2025-05-27 14:26:34 +0900

    Add: base celery setting code

--- config/celery.py
+++ config/celery.py
@@ -1,3 +1,15 @@
-from .celery import app as celery
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
 
-__all__ = ['celery']
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('config')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
