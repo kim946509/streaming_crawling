bd663d6435992de110ad9997d070111321fdfc32
민경쓰 <bmk0703@naver.com>
2025-05-27 13:00:36 +0900

    Add: Swagger url, view code

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+# ---------- ⬇️ Swagger 설정 ----------
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+    title="Streaming Platform Auto Crawling API",
+    default_version='v1',
+    description="스트리밍 플랫폼 자동 크롤링 API 문서입니다.",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('streaming_site_list.youtube.urls')),
-    path('api/v1/', include('streaming_site_list.youtube_music.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
