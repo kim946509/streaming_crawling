ccc2183f845c5d0ea608b4b418227452598f4319
민경쓰 <bmk0703@naver.com>
2025-05-27 17:13:37 +0900

    Add: song_name, youtube_url Field

--- streaming_site_list/youtube/models.py
+++ streaming_site_list/youtube/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class YouTubeSongViewCount(models.Model):
     song_id = models.CharField(max_length=255, unique=True)
+    song_name = models.CharField(max_length=255)
     view_count = models.BigIntegerField(default=0)
+    youtube_url = models.URLField(max_length=255)
     extracted_date = models.DateField()
     upload_date = models.DateField()
 
