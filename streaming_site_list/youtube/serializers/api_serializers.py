31653e30723e31f80277f2c8f1176359512075c6
민경쓰 <bmk0703@naver.com>
2025-05-27 17:12:09 +0900

    Add: song_name, youtube_url

--- streaming_site_list/youtube/serializers/api_serializers.py
+++ streaming_site_list/youtube/serializers/api_serializers.py
@@ -4,4 +4,4 @@ from streaming_site_list.youtube.models import YouTubeSongViewCount
 class YouTubeSongViewCountSerializer(serializers.ModelSerializer):
     class Meta:
         model = YouTubeSongViewCount
-        fields = ['id', 'song_id', 'view_count', 'extracted_date', 'upload_date']
+        fields = ['id', 'song_id', 'song_name', 'view_count', 'youtube_url', 'extracted_date', 'upload_date']
