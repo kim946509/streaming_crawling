73120d56b223ac15ad04c6c2d77c7df21b01d1d4
민경쓰 <bmk0703@naver.com>
2025-05-27 14:16:47 +0900

    Fix: 'api/v1/' -> 'api/v1/streaming/

--- config/urls.py
+++ config/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('streaming_site_list.youtube.urls')),
+    path('api/v1/streaming/', include('streaming_site_list.youtube.urls')),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
