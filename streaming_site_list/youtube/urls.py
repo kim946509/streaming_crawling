db97dcd2b2504ee4f885f947e399a6bff16dcc29
민경쓰 <bmk0703@naver.com>
2025-05-29 11:18:57 +0900

    Fix: import url

--- streaming_site_list/youtube/urls.py
+++ streaming_site_list/youtube/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from streaming_site_list.youtube.views.api_views import YouTubeSongViewCountAPIView
+from API_View.youtube_api_views import YouTubeSongViewCountAPIView
 
 urlpatterns = [
     path('youtube/', YouTubeSongViewCountAPIView.as_view(), name='youtube-song-view-count'), # POST, GET
