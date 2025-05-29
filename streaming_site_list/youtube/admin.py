c7b4756404d06c30b04d446f30cd82001e056257
민경쓰 <bmk0703@naver.com>
2025-05-29 11:58:06 +0900

    Update

--- streaming_site_list/youtube/admin.py
+++ streaming_site_list/youtube/admin.py
@@ -4,6 +4,5 @@ from streaming_site_list.youtube.models import YouTubeSongViewCount
 @admin.register(YouTubeSongViewCount)
 class YouTubeSongViewCountAdmin(admin.ModelAdmin):
     list_display = ('song_id', 'view_count', 'upload_date', 'extracted_date') # admin 페이지에 표시할 필드
-    search_fields = ('song_id', 'upload_date', 'extracted_date') # 검색 가능한 필드
-    
+    search_fields = ('song_id', 'song_name', 'upload_date', 'extracted_date') # 검색 가능한 필드
 
