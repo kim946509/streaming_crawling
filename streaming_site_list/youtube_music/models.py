e2c1b3ee0eb2b1a81fdb14e989f1dd4df0d7bee5
민경쓰 <bmk0703@naver.com>
2025-05-29 11:20:03 +0900

    Add: YouTubeMusicSongViewCount

---
+++ streaming_site_list/youtube_music/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class YouTubeMusicSongViewCount(models.Model):
+    song_id = models.CharField(max_length=255, unique=True)
+    song_name = models.CharField(max_length=255)
+    view_count = models.BigIntegerField(default=0)
+    youtube_music_url = models.URLField(max_length=255)
+    extracted_date = models.DateField()
+    upload_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.song_id} - {self.view_count}"
