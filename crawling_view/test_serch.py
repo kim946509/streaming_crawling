b69218f9d09e111a65914111fbafb6f7c4d433b0
민경쓰 <bmk0703@naver.com>
2025-05-30 13:05:26 +0900

    Add: crawling test code for developer

---
+++ crawling_view/test_serch.py
@@ -0,0 +1,23 @@
+from crawling_view.youtube_music_crawler_views import SearchSong, YouTubeMusicSongCrawler, save_each_to_csv
+from user_id_and_password import youtube_music_id, youtube_music_password
+
+if __name__ == "__main__":
+    search_song = SearchSong(youtube_music_id, youtube_music_password)
+    artist_name = "Jaerium"
+    company_name = "rhoonart"
+    song_names = ["Cheers to the Future", "Softness in the Snow", "The Frost of Dreams"]
+
+    artist_song_list = [(artist_name, song) for song in song_names]
+    results = search_song.search_multiple(artist_song_list)
+
+    html_list = [result['html'] for result in results]
+    info_list = YouTubeMusicSongCrawler.extract_song_info_list(html_list, artist_song_list)
+
+    for info in info_list:
+        print(f"아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 조회수: {info['view_count']}, 추출일: {info['extracted_date']}")
+
+    # CSV 저장 테스트
+    filepaths = save_each_to_csv({info['song_name']: info for info in info_list}, company_name, 'youtube_music')
+    print("저장된 파일 경로:")
+    for song, path in filepaths.items():
+        print(f"{song}: {path}")
