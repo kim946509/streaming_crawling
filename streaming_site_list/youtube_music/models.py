6051a123cae1fa8b1f972e1d6f0e29973a94168f
minkyungbae <bmk0703@naver.com>
2025-06-01 16:42:34 +0900

    Delete: upload date, youtube_music_url field

--- streaming_site_list/youtube_music/models.py
+++ streaming_site_list/youtube_music/models.py
@@ -5,9 +5,7 @@ class YouTubeMusicSongViewCount(models.Model):
     artist_name = models.CharField(max_length=255)
     song_name = models.CharField(max_length=255)
     view_count = models.BigIntegerField(default=0)
-    youtube_music_url = models.URLField(max_length=255)
     extracted_date = models.DateField()
-    upload_date = models.DateField()
 
     def __str__(self):
         return f"{self.song_id} - {self.view_count}"
--- streaming_site_list/youtube_music/youtube_music_serializers.py
+++ streaming_site_list/youtube_music/youtube_music_serializers.py
@@ -4,4 +4,4 @@ from streaming_site_list.youtube_music.models import YouTubeMusicSongViewCount
 class YouTubeMusicSongViewCountSerializer(serializers.ModelSerializer):
     class Meta:
         model = YouTubeMusicSongViewCount
-        fields = ['song_id', 'song_name', 'view_count', 'youtube_music_url', 'extracted_date', 'upload_date ']
+        fields = ['song_id', 'song_name', 'view_count', 'extracted_date']
