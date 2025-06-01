70c49b0562252488f5677bb45567f123777d7f11
minkyungbae <bmk0703@naver.com>
2025-06-01 21:07:22 +0900

    Add: test youtube, youtube_music, genie test code

--- crawling_view/test_serch.py
+++ crawling_view/test_serch.py
@@ -1,9 +1,84 @@
-from crawling_view.youtube_music_crawler_views import SearchSong, YouTubeMusicSongCrawler, save_each_to_csv
-from user_id_and_password import youtube_music_id, youtube_music_password
+import os
+import django
 
-def test_jaerium():
-    search_song = SearchSong(youtube_music_id, youtube_music_password)
-    artist_name = "Jaerium"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# from crawling_view.youtube_music_crawler_views import YouTubeMusicSearchSong, YouTubeMusicSongCrawler, save_each_to_csv as save_each_to_csv_ytmusic
+# from user_id_and_password import youtube_music_id, youtube_music_password
+from crawling_view.genie_crawler_views import GenieSearchSong, GenieSongCrawler, save_each_to_csv as save_each_to_csv_genie
+from crawling_view.youtube_crawler_views import YouTubeSongCrawler, save_each_to_csv as save_each_to_csv_youtube
+
+# '''===================== 유튜브 뮤직 테스트(jaerium) ====================='''
+# def test_jaerium_youtube_music():
+#     search_song = YouTubeMusicSearchSong(youtube_music_id, youtube_music_password)
+#     artist_name = "Jaerium"
+#     company_name = "rhoonart"
+#     song_names = [
+#         "Cheers to the Future",
+#         "Softness in the Snow",
+#         "The Frost of Dreams",
+#         "Beneath the Frozen Sky",
+#         "The Wisp of Winter",
+#         "Sparkles of the Night",
+#         "Soft Breezes in Winter",
+#         "The New Year's Moment",
+#         "Cheers to the Future",
+#         "Softness in the Snow",
+#         "The Frost of Dreams",
+#     ]
+#     artist_song_list = [(artist_name, song) for song in song_names]
+#     results = search_song.search_multiple(artist_song_list)
+#     html_list = [result['html'] for result in results]
+#     info_list = YouTubeMusicSongCrawler.extract_song_info_list(html_list, artist_song_list)
+#     for info in info_list:
+#         print(f"[YouTubeMusic] 아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 조회수: {info['view_count']}, 추출일: {info['extracted_date']}")
+#     filepaths = save_each_to_csv_ytmusic({info['song_name']: info for info in info_list}, company_name, 'youtube_music')
+#     print("저장된 파일 경로:")
+#     for song, path in filepaths.items():
+#         print(f"{song}: {path}")
+
+
+# '''===================== 유튜브 뮤직 테스트(anonatsue) ====================='''
+# def test_anonatsue_youtube_music():
+#     search_song = YouTubeMusicSearchSong(youtube_music_id, youtube_music_password)
+#     artist_name = "anonatsue"
+#     company_name = "rhoonart"
+#     song_names = [
+#         "Dreamy Orchards",
+#         "Emerald Symphony",
+#         "Sunbeam Reflections",
+#         "Warm Breeze Serenade",
+#         "Velvet Dawn",
+#         "Enchanted Bloom",
+#         "Lush Springtime",
+#         "Cascading Waterfall",
+#         "Fragrant Twilight",
+#         "Cherry Blossom Serenade",
+#         "Soft Petal Waltz",
+#         "Garden of Serenity",
+#         "Wind's Caress",
+#         "Secret Garden Lullaby",
+#         "Azure Morning",
+#         "Lush Green Fields",
+#         "Meadow Whispers",
+#     ]
+#     artist_song_list = [(artist_name, song) for song in song_names]
+#     results = search_song.search_multiple(artist_song_list)
+#     html_list = [result['html'] for result in results]
+#     info_list = YouTubeMusicSongCrawler.extract_song_info_list(html_list, artist_song_list)
+#     for info in info_list:
+#         print(f"[YouTubeMusic] 아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 조회수: {info['view_count']}, 추출일: {info['extracted_date']}")
+#     filepaths = save_each_to_csv_ytmusic({info['song_name']: info for info in info_list}, company_name, 'youtube_music')
+#     print("저장된 파일 경로:")
+#     for song, path in filepaths.items():
+#         print(f"{song}: {path}")
+
+
+'''===================== 지니 테스트(jaerium) ====================='''
+def test_genie():
+    search_song = GenieSearchSong()
+    artist_name = "제이리움"
     company_name = "rhoonart"
     song_names = [
         "Cheers to the Future",
@@ -16,59 +91,59 @@ def test_jaerium():
         "The New Year's Moment",
         "Cheers to the Future",
         "Softness in the Snow",
-        "The Frost of Dreams",
-    ]
+        "The Frost of Dreams",]
     artist_song_list = [(artist_name, song) for song in song_names]
     results = search_song.search_multiple(artist_song_list)
     html_list = [result['html'] for result in results]
-    info_list = YouTubeMusicSongCrawler.extract_song_info_list(html_list, artist_song_list)
+    info_list = GenieSongCrawler.crawl(html_list, artist_song_list)
     for info in info_list:
-        print(f"아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 조회수: {info['view_count']}, 추출일: {info['extracted_date']}")
-    filepaths = save_each_to_csv({info['song_name']: info for info in info_list}, company_name, 'youtube_music')
+        print(f"[Genie] 아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 전체 청취자수: {info['total_person_count']}, 전체 재생수: {info['total_play_count']}, 추출일: {info['extracted_date']}")
+    filepaths = save_each_to_csv_genie({info['song_name']: info for info in info_list}, company_name, 'genie')
     print("저장된 파일 경로:")
     for song, path in filepaths.items():
         print(f"{song}: {path}")
 
-def test_anonatsue():
-    search_song = SearchSong(youtube_music_id, youtube_music_password)
-    artist_name = "anonatsue"
+
+'''===================== 유튜브 테스트 ====================='''
+def test_youtube():
     company_name = "rhoonart"
-    song_names = [
-        "Dreamy Orchards",
-        "Emerald Symphony",
-        "Sunbeam Reflections",
-        "Warm Breeze Serenade",
-        "Velvet Dawn",
-        "Enchanted Bloom",
-        "Lush Springtime",
-        "Cascading Waterfall",
-        "Fragrant Twilight",
-        "Cherry Blossom Serenade",
-        "Soft Petal Waltz",
-        "Garden of Serenity",
-        "Wind's Caress",
-        "Secret Garden Lullaby",
-        "Azure Morning",
-        "Lush Green Fields",
-        "Meadow Whispers",
+    song_urls = [
+        "https://www.youtube.com/watch?v=6hhhleiuaJA",
+        "https://www.youtube.com/watch?v=jKY7pm7xlLk",
+        "https://www.youtube.com/watch?v=C36Y5fmPnrQ",
+        "https://www.youtube.com/watch?v=cpfFpC5xrrY",
+        "https://www.youtube.com/watch?v=TlkHKmjha3U",
+        "https://www.youtube.com/watch?v=M1MFK5rWUpU",
+        "https://www.youtube.com/watch?v=LDJAuOW-_-4", 
+        "https://www.youtube.com/watch?v=z7WJw6SY0m0", 
+        "https://www.youtube.com/watch?v=2r0Wh1uEiuE", 
+        "https://www.youtube.com/watch?v=R6VH1qB-Hlg", 
+        "https://www.youtube.com/watch?v=HSUgcYisbmw", 
+        "https://www.youtube.com/watch?v=fi-QYKZP1d0", 
+        "https://www.youtube.com/watch?v=uIcpEprBKUA", 
+        "https://www.youtube.com/watch?v=r8clc_Vwahs", 
+        "https://www.youtube.com/watch?v=jn__gJ-7-vE", 
+        "https://www.youtube.com/watch?v=61yiWvXwB74", 
+        "https://www.youtube.com/watch?v=Dz8dI9G-kMk"
     ]
-    artist_song_list = [(artist_name, song) for song in song_names]
-    results = search_song.search_multiple(artist_song_list)
-
-    html_list = [result['html'] for result in results]
-    info_list = YouTubeMusicSongCrawler.extract_song_info_list(html_list, artist_song_list)
-
-    for info in info_list:
-        print(f"아티스트: {info['artist_name']}, 곡명: {info['song_name']}, 조회수: {info['view_count']}, 추출일: {info['extracted_date']}")
-
-    # CSV 저장 테스트
-    filepaths = save_each_to_csv({info['song_name']: info for info in info_list}, company_name, 'youtube_music')
+    results = YouTubeSongCrawler(song_urls)
+    for song_id, info in results.items():
+        print(f"[YouTube] 곡명: {info['song_name']}, 조회수: {info['view_count']}, URL: {info['youtube_url']}, 업로드일: {info['upload_date']}, 추출일: {info['extracted_date']}")
+    filepaths = save_each_to_csv_youtube(results, company_name, 'youtube')
     print("저장된 파일 경로:")
     for song, path in filepaths.items():
         print(f"{song}: {path}")
+
+
 
 if __name__ == "__main__":
-    print("===== Jaerium 테스트 =====")
-    test_jaerium()
-    print("\n===== Anonatsue 테스트 =====")
-    test_anonatsue()
+    # print("\n===== YouTubeMusic(Jaerium) 테스트 =====")
+    # test_jaerium_youtube_music()
+    # print("\n===== YouTubeMusic(Anonatsue) 테스트 =====")
+    # test_anonatsue_youtube_music()
+
+    print("\n===== Genie 테스트 =====")
+    test_genie()
+
+    print("\n===== YouTube 테스트 =====")
+    test_youtube()
