0b30fb766e4586e905c49054e73175a6492a3dc2
minkyungbae <bmk0703@naver.com>
2025-06-01 22:04:20 +0900

    Migration: delete song_id

--- streaming_site_list/youtube_music/admin.py
+++ streaming_site_list/youtube_music/admin.py
@@ -3,5 +3,5 @@ from streaming_site_list.youtube_music.models import YouTubeMusicSongViewCount
 
 @admin.register(YouTubeMusicSongViewCount)
 class YouTubeMusicSongViewCountAdmin(admin.ModelAdmin):
-    list_display = ('song_id', 'view_count', 'extracted_date') # admin 페이지에 표시할 필드
-    search_fields = ('song_id', 'song_name', 'extracted_date') # 검색 가능한 필드
+    list_display = ('artist_name', 'song_name', 'view_count', 'extracted_date') # admin 페이지에 표시할 필드
+    search_fields = ('artist_name', 'song_name', 'extracted_date') # 검색 가능한 필드
