8d0b8fd0c1d0803051eae47b4255c784d6836c2b
minkyungbae <bmk0703@naver.com>
2025-06-01 22:16:04 +0900

    Reapply "Migration: delete song_id"
    This reverts commit 4ac96e22da5db288a5409cdd332f2560271e7cbf.

--- streaming_site_list/youtube_music/models.py
+++ streaming_site_list/youtube_music/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class YouTubeMusicSongViewCount(models.Model):
-    song_id = models.CharField(max_length=255, unique=True)
     artist_name = models.CharField(max_length=255)
     song_name = models.CharField(max_length=255)
     view_count = models.BigIntegerField(default=0)
     extracted_date = models.DateField()
 
     def __str__(self):
-        return f"{self.song_id} - {self.view_count}"
+        return f"{self.artist_name} - {self.song_name}"
