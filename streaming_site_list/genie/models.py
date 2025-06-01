0b4bcdc7d274ba65e0d0acc712ebf821961bac22
minkyungbae <bmk0703@naver.com>
2025-06-01 22:15:07 +0900

    Reapply "Migration: delete song_id"
    This reverts commit f89a34c0160233b50ec96c79ae8b61874a1caf61.

--- streaming_site_list/genie/models.py
+++ streaming_site_list/genie/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class GenieSongViewCount(models.Model):
-    song_id = models.CharField(max_length=255, unique=True)
     artist_name = models.CharField(max_length=255)
     song_name = models.CharField(max_length=255)
     total_person_count = models.BigIntegerField(default=0)
@@ -9,4 +8,4 @@ class GenieSongViewCount(models.Model):
     extracted_date = models.DateField()
 
     def __str__(self):
-        return f"{self.song_id} - {self.view_count}"
+        return f"{self.artist_name} - {self.song_name}"
