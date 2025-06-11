304a0e7bb05d446178ca110ccdf3119f26de62bb
minkyungbae <bmk0703@naver.com>
2025-06-11 15:20:14 +0900

    Add: youtube url field

--- streaming_site_list/youtube/admin.py
+++ streaming_site_list/youtube/admin.py
@@ -3,6 +3,6 @@ from streaming_site_list.youtube.models import YouTubeSongViewCount
 
 @admin.register(YouTubeSongViewCount)
 class YouTubeSongViewCountAdmin(admin.ModelAdmin):
-    list_display = ('song_name', 'view_count', 'upload_date', 'extracted_date') # admin 페이지에 표시할 필드
-    search_fields = ('song_name', 'upload_date', 'extracted_date') # 검색 가능한 필드
+    list_display = ('song_name', 'view_count', 'youtube_url', 'upload_date', 'extracted_date') # admin 페이지에 표시할 필드
+    search_fields = ('song_name', 'youtube_url', 'upload_date', 'extracted_date') # 검색 가능한 필드
     
